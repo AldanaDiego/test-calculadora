8e449af5bd645209b852b2a05d80d75b1b0611c7
unknown <daldana9@gmail.com>
2020-01-10 16:34:45 -0300

    Implementacion de operaciones aritmeticas basicas

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,29 @@
+import calculadora.Calculadora;
+
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        Calculadora calc = new Calculadora();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese una operacion (Ej: 2 + 3)");
+        String input = sc.nextLine().replaceAll(" ", "");
+        String[] op = input.split("/+|-|/*|//");
+
+        float result = 0;
+        if(op[1].equals("+")){
+            result = calc.suma(Float.parseFloat(op[0]), Float.parseFloat(op[2]));
+        }
+        else if(op[1].equals("-")){
+            result = calc.resta(Float.parseFloat(op[0]), Float.parseFloat(op[2]));
+        }
+        else if(op[1].equals("*")){
+            result = calc.mult(Float.parseFloat(op[0]), Float.parseFloat(op[2]));
+        }
+        else if(op[1].equals("/")){
+            result = calc.suma(Float.parseFloat(op[0]), Float.parseFloat(op[2]));
+        }
+        System.out.println(result);
     }
 }
